5f756917f7627413195b8f007278039c303e81c6
austinhays <ahays.target@yahoo.com>
2016-06-09 14:05:16 -0400

    HelloSpark Initial Import

---
+++ src/com/theironyard/User.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+/**
+ * Created by austinhays on 6/9/16.
+ */
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
